aa92dc92201c5b532eb07e923744427bfc3e79eb
Chistov Sergey Vadimovich <sergeychistov4088@gmail.com>
2021-03-16 01:16:57 +0700

    fixed

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 from rest_framework.routers import DefaultRouter
+
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='post')
 router.register(r'posts/(?P<post_id>\d+)/comments',
@@ -13,16 +12,4 @@ router.register('group', GroupViewSet, basename='group')
 router.register('follow', FollowViewSet, basename='follow')
 urlpatterns = [
     path('v1/', include(router.urls)),
-]
-urlpatterns = [
-    path(
-        'api/v1/token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path(
-        'api/v1/token/refresh/',
-        TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
 ]
